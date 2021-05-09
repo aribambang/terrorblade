0c70c8ecbfdc7ecbdcebca954b735af0a2d0f014
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-05-10 00:27:57 +0700

    state with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom';
+import GuestList from './state/GuestList';
 
 const App = () => {
-  return <div><h1>Hi there!</h1></div>
-}
+  return (
+    <div>
+      <GuestList />
+    </div>
+  );
+};
 
-ReactDom.render(<App />, document.querySelector('#root'))
+ReactDom.render(<App />, document.querySelector('#root'));
